ec65ee27ba3df856badd5ea911e30e9c024350d8
unknown <mb1069@PC603S.its.york.ac.uk>
2015-11-13 18:45:20 +0000

    Implemented single channel callback, requires additional work to avoid double-timing events

--- src/lsi/wsn/sync/MyNode.java
+++ src/lsi/wsn/sync/MyNode.java
@@ -1,13 +1,13 @@
 package lsi.wsn.sync;
-
+//   lsi.wsn.sync.MyNode
 import ptolemy.actor.IOPort;
+import ptolemy.actor.NoTokenException;
 import ptolemy.actor.TypedAtomicActor;
 import ptolemy.actor.TypedIOPort;
 import ptolemy.actor.util.Time;
-import ptolemy.data.IntToken;
-import ptolemy.data.LongToken;
-import ptolemy.data.RecordToken;
-import ptolemy.data.Token;
+import ptolemy.data.*;
+import ptolemy.data.type.BaseType;
+import ptolemy.data.type.Type;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
@@ -15,14 +15,15 @@ import ptolemy.kernel.util.NameDuplicationException;
 public class MyNode extends TypedAtomicActor{
     protected TypedIOPort input;
     protected TypedIOPort output;
-    protected Time time;
-    protected int[] channels = {11,12,13,14,15};
+    protected TypedIOPort feedbackOutput;
+    protected Time lastTime;
+    protected int lastCount = 0;
+    protected boolean fire = false;
     public MyNode(CompositeEntity container, String name) throws IllegalActionException, NameDuplicationException {
         super(container, name);
-        time =  getDirector().getModelTime();
         input = new TypedIOPort(this, "input", true, false);
         output = new TypedIOPort(this, "output", false, true);
-
+        feedbackOutput = new TypedIOPort(this, "output2", false, true);
     }
 
     @Override
@@ -34,11 +35,31 @@ public class MyNode extends TypedAtomicActor{
     @Override
     public void fire() throws IllegalActionException {
         super.fire();
-        getDirector().fireAt(this, getDirector().getModelTime().add(20));
-        LongToken time = new LongToken(getDirector().getModelTime().getLongValue());
+        if (fire){
+            fire = false;
+            feedbackOutput.broadcast(new IntToken(666));
+        } else {
+            try {
+                Token countToken = input.get(0);
+                int count = ((IntToken) countToken).intValue();
+                if (lastCount == 0){
+                    lastTime = getDirector().getModelTime();
+                    lastCount = count;
+                } else {
+                    Time broadcastTime = predictBroadcastTime(lastCount, lastTime, count, getDirector().getModelTime());
+                    fire = true;
+                    //Add method to prevent firing multiple events at same channel simultaneously, and keep track of which record to next fire
+                    getDirector().fireAt(this, broadcastTime);
+                }
+                output.broadcast(countToken);
+                System.out.println(getDirector().getModelTime() + " : " + count);
+            } catch( NoTokenException e){}
+        }
+    }
 
-        Token count = input.get(0);
-        RecordToken token = new RecordToken(new String[]{"channel", "time", "count"}, new Token[]{new IntToken(11), time, count});
-        output.broadcast(token);
+    private Time predictBroadcastTime(int firstCount, Time firstTime, int secondCount, Time secondTime){
+        int countDiff = secondCount-firstCount;
+        long period = (firstTime.subtract(secondTime)).getLongValue()/countDiff;
+        return new Time(getDirector(), (period*secondCount)).add(secondTime);
     }
 }
