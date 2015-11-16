276f44a738e38ef88ea3e2c0f62d38f743917429
unknown <mb1069@PC813S.its.york.ac.uk>
2015-11-16 14:15:49 +0000

    Functional on a single channel

--- src/lsi/wsn/sync/MyNode.java
+++ src/lsi/wsn/sync/MyNode.java
@@ -12,13 +12,21 @@ import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 
-public class MyNode extends TypedAtomicActor{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MyNode extends TypedAtomicActor {
     protected TypedIOPort input;
     protected TypedIOPort output;
     protected TypedIOPort feedbackOutput;
-    protected Time lastTime;
-    protected int lastCount = 0;
-    protected boolean fire = false;
+    protected HashMap<Time, Integer> firingTimes = new HashMap<Time, Integer>();
+    protected ArrayList<Time> times = new ArrayList<Time>();
+    protected ArrayList<Integer> counts = new ArrayList<Integer>();
+    protected int currentChannel = 11;
+
+
+
     public MyNode(CompositeEntity container, String name) throws IllegalActionException, NameDuplicationException {
         super(container, name);
         input = new TypedIOPort(this, "input", true, false);
@@ -29,37 +37,53 @@ public class MyNode extends TypedAtomicActor{
     @Override
     public void initialize() throws IllegalActionException {
         super.initialize();
-        getDirector().fireAt(this, getDirector().getModelTime().add(20));
     }
 
     @Override
     public void fire() throws IllegalActionException {
         super.fire();
-        if (fire){
-            fire = false;
+        Time currentTime = getDirector().getModelTime();
+        if (firingTimes.keySet().contains(currentTime)) {
+            System.out.println('a');
+            //TODO set channel
             feedbackOutput.broadcast(new IntToken(666));
+            System.out.println("Firing!! Time is: " + currentTime);
+            firingTimes.remove(currentTime);
+
         } else {
             try {
                 Token countToken = input.get(0);
+                Time time = getDirector().getModelTime();
                 int count = ((IntToken) countToken).intValue();
-                if (lastCount == 0 && count!=lastCount){
-                    lastTime = getDirector().getModelTime();
-                    lastCount = count;
-                } else {
-                    Time broadcastTime = predictBroadcastTime(lastCount, lastTime, count, getDirector().getModelTime());
-                    fire = true;
-                    //Add method to prevent firing multiple events at same channel simultaneously, and keep track of which record to next fire
-                    getDirector().fireAt(this, broadcastTime);
+                System.out.println(getDirector().getModelTime() + " : " + count);
+
+                if (!firingTimes.values().contains(currentChannel)) {
+                    counts.add(count);
+                    times.add(time);
+                    if (counts.size() == 2) {
+                        Time broadcastTime = predictBroadcastTime(counts, times);
+                        //Add method to prevent firing multiple events at same channel simultaneously, and keep track of which record to next fire
+                        getDirector().fireAt(this, broadcastTime);
+                        System.out.println("Firing at: " + broadcastTime);
+                        firingTimes.put(broadcastTime, currentChannel);
+                        counts = new ArrayList<Integer>();
+                        times = new ArrayList<Time>();
+                    }
                 }
                 output.broadcast(countToken);
-                System.out.println(getDirector().getModelTime() + " : " + count);
-            } catch( NoTokenException e){}
+            } catch (NoTokenException e) {
+            }
         }
     }
 
-    private Time predictBroadcastTime(int firstCount, Time firstTime, int secondCount, Time secondTime){
-        int countDiff = secondCount-firstCount;
-        long period = (firstTime.subtract(secondTime)).getLongValue()/countDiff;
-        return new Time(getDirector(), (period*secondCount)).add(secondTime);
+    private Time predictBroadcastTime(List<Integer> counts, List<Time> times) {
+        int countDiff = counts.get(0) - counts.get(1);
+        long longTime = ((times.get(1).subtract(times.get(0)).getLongValue()) / countDiff);
+        System.out.println("Period: " + new Time(getDirector(), longTime));
+        return new Time(getDirector(), (longTime * counts.get(1))).add(times.get(1));
+    }
+
+    private void toggle(boolean[] b, int index) {
+        b[index] = !b[index];
     }
 }
